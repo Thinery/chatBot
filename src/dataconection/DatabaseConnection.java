ed81e96f9726267bbe3151b0da94668059be6f33
Thiago Nery <thi.nery7@gmail.com>
2025-01-02 09:46:18 -0300

    introduzindo o banco de dados

---
+++ src/dataconection/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package dataconection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	private static final String URL = "jdbc:mysql://localhost:3306/dbconsulta";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
